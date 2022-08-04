e77a01f10edd703289a20b94ae29a140b17bcdd5
anveshdasari <101214244+anveshdasari@users.noreply.github.com>
2022-08-03 22:13:04 -0400

    Create Sventh.java

---
+++ Sventh.java
@@ -0,0 +1,13 @@
+
+public class Sixth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Testing ");
+		System.out.println("Java ");
+		System.out.println("Program");
+    System.out.println("Seventh");
+	}
+
+}
